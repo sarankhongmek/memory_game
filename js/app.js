01c7cc2b1620c9445913f07c70cc906b40076e81
Saran-Khongmek <James.warrender1@btinternet.com>
2018-07-25 19:59:26 +0100

    open card to show for 1 sec

--- js/app.js
+++ js/app.js
@@ -42,15 +42,25 @@ var openCards = [];
 allCards.forEach(function(card){
   card.addEventListener('click', function(e){
 
-    if (openCards.length > 2){
-      openCards.forEach(function(card){
-        card.classList.remove('open', 'show');
-        openCards = [];
-      });
 
-    } else {
-      card.classList.add('open','show');
+
+    if(!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
       openCards.push(card);
+      card.classList.add('open','show');
+
+      if (openCards.length == 2) {
+          setTimeout(function() {
+            openCards.forEach(function (card){
+              card.classList.remove('open','show');
+            });
+            openCards = [];
+        },1000);
+      }
     }
+
+
+
+
+
   });
 });
